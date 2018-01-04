c88540d9d1271353ed26da326a42abc7cb64f131
Andres Sancho <andres.sancho@asseco.local>
2018-01-04 13:08:10 +0100

    Segunda práctica

--- Angular2/2. SPA/src/app/app.module.ts
+++ Angular2/2. SPA/src/app/app.module.ts
@@ -3,18 +3,12 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { BodyComponent } from './components/body/body.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    BodyComponent,
-    FooterComponent,
     NavbarComponent
   ],
   imports: [
