ddcf43bd3c4eecdf7ec422868cba9395f8bd5603
Andres Sancho <andres.sancho@asseco.local>
2018-01-04 09:28:09 +0100

    limpiar aplicaciones

--- prueba1/app.js
+++
@@ -1,7 +0,0 @@
-function prueba(nombre) {
-    console.log(nombre);
-}
-var objeto = {
-    nombre: "ddzcbzdbvd"
-};
-prueba(objeto.nombre);
--- prueba2/app.js
+++
@@ -1,7 +0,0 @@
-var variableNormal = "hola1";
-var variable = "hola2";
-if (true) {
-    var variableNormal = "adios1";
-    var variable_1 = "adios2";
-}
-console.log(variableNormal + " " + variable);
