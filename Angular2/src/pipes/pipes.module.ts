ed3970966e27c3a6b56253eca8f7f9a7747958f7
Andres Sancho <andres.sancho@asseco.local>
2018-01-09 16:35:47 +0100

    src

--- Angular2/src/pipes/pipes.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { PlaceholderPipe } from './placeholder/placeholder';
-@NgModule({
-	declarations: [PlaceholderPipe],
-	imports: [],
-	exports: [PlaceholderPipe]
-})
-export class PipesModule {}
