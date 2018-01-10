923a3b60eae5e56624a4fa9e33f89af0b6461bed
Andres Sancho <andres.sancho@asseco.local>
2018-01-10 17:02:07 +0100

    uso de ngStyle, ngClass y rutas hijas con paso de parámetros

---
+++ Angular2/6. Misceláneos/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { APP_ROUTING } from './app.routes';
+import { AppComponent } from './app.component';
+import { NgStyleComponent } from './componentes/ng-style/ng-style.component';
+import { CssComponent } from './componentes/css/css.component';
+import { ClasesComponent } from './componentes/clases/clases.component';
+import { ResaltadoDirective } from './directives/resaltado.directive';
+import { NgSwitchComponent } from './componentes/ng-switch/ng-switch.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { UsuarioComponent } from './componentes/usuario/usuario.component';
+import { UsuarioNuevoComponent } from './componentes/usuario/usuario-nuevo.component';
+import { UsuarioEditarComponent } from './componentes/usuario/usuario-editar.component';
+import { UsuarioDetalleComponent } from './componentes/usuario/usuario-detalle.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NgStyleComponent,
+    CssComponent,
+    ClasesComponent,
+    ResaltadoDirective,
+    NgSwitchComponent,
+    HomeComponent,
+    UsuarioComponent,
+    UsuarioNuevoComponent,
+    UsuarioEditarComponent,
+    UsuarioDetalleComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    APP_ROUTING
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Angular2/6. Misceláneos/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { HomeComponent } from './componentes/home/home.component';
+import { UsuarioComponent } from './componentes/usuario/usuario.component';
+import { RouterModule, Routes } from '@angular/router';
+import { USUARIO_ROUTES } from './componentes/usuario/usuario.routes';
+const APP_ROUTES: Routes =[
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'usuario/:id', component: UsuarioComponent,
+    children: USUARIO_ROUTES
+  },
+  { path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, {useHash: true});
