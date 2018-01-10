f1a350e15240ff7864b5611604c707fad590a2e8
Andres Sancho <andres.sancho@asseco.local>
2018-01-10 17:15:44 +0100

    ciclo de vida de un componente

--- Angular2/6. Misceláneos/src/app/componentes/home/home.component.ts
+++ Angular2/6. Misceláneos/src/app/componentes/home/home.component.ts
@@ -1,21 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,
+  //Ciclos de vida de un componente:
+    OnChanges, OnInit, DoCheck,
+    AfterContentInit, AfterContentChecked,
+    AfterViewInit, AfterViewChecked, OnDestroy
+  } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   template: `
-    <app-ng-style></app-ng-style>
-    <app-css></app-css>
-    <app-clases></app-clases>
-    <p [appResaltado]="'red'">Hola mundo desde app-component</p>
+    <app-ng-style></app-ng-style><br>
+    <app-css></app-css><br>
+    <app-clases></app-clases><br><br>
+    <p [appResaltado]="'red'">Hola mundo desde app-component</p><br>
     <app-ng-switch></app-ng-switch>
   `,
   styles: []
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnChanges, OnInit, DoCheck,
+                                    AfterContentInit, AfterContentChecked,
+                                    AfterViewInit, AfterViewChecked, OnDestroy {
 
-  constructor() { }
+  constructor() {
+    console.log("constructor(){}");
+  }
 
-  ngOnInit() {
+  ngOnChanges(){
+    console.log("ngOnChanges(){}");
+  }
+  
+  ngOnInit(){
+    console.log("ngOnInit(){}");
+  }
+  
+  ngDoCheck(){
+    console.log("ngDoCheck(){}");
+  }
+  
+  ngAfterContentInit(){
+    console.log("ngAfterContentInit(){}");
+  }
+  
+  ngAfterContentChecked(){
+    console.log("ngAfterContentChecked(){}");
+  }
+  
+  ngAfterViewInit(){
+    console.log("ngAfterViewInit(){}");
+  }
+  
+  ngAfterViewChecked(){
+    console.log("ngAfterViewChecked(){}");
+  }
+  
+  ngOnDestroy(){
+    console.log("ngOnDestroy(){}");
   }
 
 }
